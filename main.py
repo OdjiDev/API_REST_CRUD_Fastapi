43ff7e109dc16e9848a54d8f0a6dde4ab3c44862
Djire <odjidev@gmail.com>
2026-06-03 09:59:46 +0000

    Creation  de quelque route get dans le fichier mais pour tester

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"Hello": "World"}
+@app.get("/cours")
+def cours():
+    return {"cours": "FastAPI"}
+@app.get("/etudiants")
+def etudiants():
+    return {"Noombre d'etudiants": "100"}
+@app.get("/enseignants")
+def enseignants():
+    return {"enseignants": "DJIRE OUZAIROU"}
+@app.get("/departements")
+def departements():
+    return {"departements": "Informatique"}
